98e19a19fb117f3fcda8500f6532e9bab239f5b5
Ercius XPS-13 sourcetree <percius@lbl.gov>
2019-03-04 10:54:10 -0800

    Half working. I get the data now. I need to implement the ROI and the FFT view

--- xicam/NCEM/widgets/FFTViewerPlugin.py
+++ xicam/NCEM/widgets/FFTViewerPlugin.py
@@ -1,52 +1,60 @@
 from xicam.plugins import QWidgetPlugin
-from pyqtgraph import ImageView, PlotItem
+from xicam.core import msg
+from xicam.gui.widgets.dynimageview import DynImageView
 from xicam.core.data import NonDBHeader
+
+from pyqtgraph import ImageView, PlotItem
+import pyqtgraph as pg
+
 from qtpy.QtWidgets import *
 from qtpy.QtCore import *
 from qtpy.QtGui import *
 from qtpy.QtCore import QRect, QRectF
 
 import numpy as np
-from xicam.core import msg
-from xicam.gui.widgets.dynimageview import DynImageView
-#import pyqtgraph as pg
 
-# TODO: single-source with SAXSViewerPlugin
-
-
-class FFTViewerPlugin(QWidgetPlugin, QWidgetPlugin):
+class FFTViewerPlugin(QWidgetPlugin):
     def __init__(self, header: NonDBHeader = None, field: str = 'primary', toolbar: QToolBar = None, *args, **kwargs):
         
-        #Two Dynamic image views (maybe only need 1 for the main data. The FFT can be an ImageView()
+        super(FFTViewerPlugin, self).__init__(**kwargs)
+        
         self.Rimageview = DynImageView()
-        self.Fimageview = DynImageView()
-        # Keep Y-axis as is
-        self.Rimageview.view.invertY(True)
-        self.Fimageview.view.invertY(True)
-        self.Rimageview.imageItem.setOpts(axisOrder='col-major')
-        self.Fimageview.imageItem.setOpts(axisOrder='col-major')
+        self.Fimageview = ImageView()
         
-        # Add axes to the main data
-        self.axesItem = PlotItem()
-        # self.axesItem.setLabel('bottom', u'q ()')  # , units='s')
-        # self.axesItem.setLabel('left', u'q ()')
-        self.axesItem.axes['left']['item'].setZValue(10)
-        self.axesItem.axes['top']['item'].setZValue(10)
-        if 'view' not in kwargs: kwargs['view'] = self.axesItem
-        
-        #Add to a layout
         self.setLayout(QHBoxLayout())
         self.layout().addWidget(self.Rimageview)
         self.layout().addWidget(self.Fimageview)
         
+        # Add axes to the main data
+        #self.axesItem = PlotItem()
+        #self.axesItem.setLabel('bottom', u'q ()')  # , units='s')
+        #self.axesItem.setLabel('left', u'q ()')
+        #self.axesItem.axes['left']['item'].setZValue(10)
+        #self.axesItem.axes['top']['item'].setZValue(10)
+        #if 'view' not in kwargs: kwargs['view'] = self.axesItem
+        #self.Rimageview = DynImageView(view=self.axesItem)
+        
+        #Two Dynamic image views (maybe only need 1 for the main data. The FFT can be an ImageView()
+        #self.Rimageview = DynImageView()
+        #self.Fimageview = ImageView()
+        # Keep Y-axis as is
+        #self.Rimageview.view.invertY(True)
+        #self.Fimageview.view.invertY(True)
+        #self.Rimageview.imageItem.setOpts(axisOrder='col-major')
+        #self.Fimageview.imageItem.setOpts(axisOrder='col-major')
+        
+        
+        #self.layout().addWidget(self.Rimageview)
+        #self.layout().addWidget(self.Fimageview)
+        
         #Add ROI to real image
-        self.Rroi = pg.RectROI(pos=(0, 0), size=(10, 10), translateSnap=True, snapSize=1, scaleSnap=True)
-        self.Rroi.sigRegionChanged.connect(self.updateFFT)
-        Rview = self.Rimageview.view  # type: pg.ViewBox
-        Rview.addItem(self.Rroi)
+        #self.Rroi = pg.RectROI(pos=(0, 0), size=(10, 10), translateSnap=True, snapSize=1, scaleSnap=True)
+        #self.Rroi.sigRegionChanged.connect(self.updateFFT)
+        #Rview = self.Rimageview.view  # type: pg.ViewBox
+        #Rview.addItem(self.Rroi)
         
-        super(FFTViewerPlugin, self).__init__(**kwargs)
         
+        '''
         # Setup axes reset button 
         self.resetAxesBtn = QPushButton('Reset Axes')
         sizePolicy = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Fixed)
@@ -77,7 +85,8 @@ class FFTViewerPlugin(QWidgetPlugin, QWidgetPlugin):
         # Setup coordinates label
         self.coordinatesLbl = QLabel('--COORDINATES WILL GO HERE--')
         self.ui.gridLayout.addWidget(self.coordinatesLbl, 3, 0, 1, 1, alignment=Qt.AlignHCenter)
-
+        '''
+        
         # Set header
         if header: self.setHeader(header, field)
     
@@ -86,8 +95,21 @@ class FFTViewerPlugin(QWidgetPlugin, QWidgetPlugin):
         ROI location and size
         
         '''
-        fft = np.abs(np.fft.fft2(data[int(self.Rroi.pos().x()):int(self.Rroi.pos().x() + self.Rroi.size().x()),int(self.Rroi.pos().y()):int(self.Rroi.pos().y() + self.Rroi.size().y())] ))
-        self.Fimageview.setImage(np.log(np.abs(np.fft.fftshift(fft))+ 1))
+        msg.logMessage('0 = {}'.format(int(self.Rroi.pos().x())))
+        msg.logMessage('1 = {}'.format(int(self.Rroi.pos().y())))
+        msg.logMessage('2 = {}'.format(int(self.Rroi.size().x())))
+        msg.logMessage('3 = {}'.format(int(self.Rroi.size().y())))
+        x0 = int(self.Rroi.pos().x())
+        x1 = int(self.Rroi.pos().x()) + int(self.Rroi.size().x())
+        y0 = int(self.Rroi.pos().y())
+        y1 = int(self.Rroi.pos().y()) + int(self.Rroi.size().y())
+        msg.logMessage('type data = {}'.format(type(data)))
+        
+        temp = data[x0:x1,y0:y1]
+        fft = np.fft.fft2(temp)
+        #fft = np.fft.fft2(data[int(self.Rroi.pos().x()):int(self.Rroi.pos().x() + self.Rroi.size().x()),
+        #                       int(self.Rroi.pos().y()):int(self.Rroi.pos().y() + self.Rroi.size().y())] )
+        self.Fimageview.setImage(np.log(np.abs(np.fft.fftshift(fft)) + 1))
     
     def setHeader(self, header: NonDBHeader, field: str, *args, **kwargs):
         self.header = header
@@ -113,12 +135,13 @@ class FFTViewerPlugin(QWidgetPlugin, QWidgetPlugin):
                     scale0 = [1, 1]
                     units0 = ['', '']
                     #msg.logMessage{'NCEMviewer: No pixel size or units detected'}
-                super(NCEMViewerPlugin, self).setImage(img=data, scale=scale0, *args, **kwargs)
+                self.Rimageview.setImage(img=data, scale=scale0, *args, **kwargs)
+                
                 # TODO: Need to add axesitem to the DynImageView
-                self.axesItem.setLabel('bottom', text='X', units=units0[0])
-                self.axesItem.setLabel('left', text='Y', units=units0[1])
+                #self.axesItem.setLabel('bottom', text='X', units=units0[0])
+                #self.axesItem.setLabel('left', text='Y', units=units0[1])
                 
                 #Update the FFT of the image when new data is shown
-                self.updateFFT(data)
+                #self.updateFFT(data['data'])
             #else:
             #    msg.logMessage('Cant load 1D data.')
